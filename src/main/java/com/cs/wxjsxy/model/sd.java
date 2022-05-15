05de92a24609c16b6aa08d750b6c66ee53edb23e
yjxy <yjxyml@qq.com>
2022-05-15 15:11:43 +0800

    c1

--- src/main/java/com/cs/wxjsxy/model/sd.java
+++ src/main/java/com/cs/wxjsxy/model/sd.java
@@ -4,5 +4,6 @@ public class sd {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("dafas");
+        System.out.println("dvac1");
     }
 }
